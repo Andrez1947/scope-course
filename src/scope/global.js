c2b227f0ab1b9a6a52281de3d7e44a8627670426
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-03-15 21:38:39 -0500

    beginning scope understantding

---
+++ src/scope/global.js
@@ -0,0 +1,16 @@
+//variables
+
+var a; //declarando
+var b = 'b'; //declaramos / asignamos
+b = 'bb'; // reasignación
+var a = 'aa'; //redeclaración
+
+//Global scope
+var fruit = 'apple'; //Global Scope
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
+
