8f75d2b4e99ac3778fd8e8d7101c5dfd49e39bb3
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-03-24 15:43:49 -0500

    ascope re ra

--- src/scope/re.js
+++ src/scope/re.js
@@ -1,3 +1,4 @@
+//Usando var puedo declarar, asignar, redeclaras y reasignar:
 var firstName; //Undefined
 firstName = 'Saul Hermano'; //Asignando
 console.log(firstName);
@@ -9,3 +10,26 @@ console.log(lastName);
 var secondName = 'Jimmy'; //Declarar y asignar
 var secondName = 'Kim'; //Reasignando
 console.log(secondName);
+
+//Usando let puedo declarar, asignar y reasignar:
+
+let fruit; // declarando
+
+fruit = 'Banana'; // Asignando
+fruit = 'Orange'; //Reasignar
+
+console.log(fruit);
+
+let fruit = 'Pinneaple'; //Redeclarar (error)
+
+//Const: No puedo declarar ningún tipo e dato. No puedo redeclarar ni reasignar datos primitivos (Integer, String, Undefined, Null, Symbol, Boolean). Puedo reasignar pero NO redeclarar arrays y objetos:
+
+const animal = 'dog'; //Declarando y asignando
+
+const animal = 'Snake' //No puedo redeclarar
+
+const vehicles = []; //Declarando y asignando const
+vehicles.push('🚗'); //Reasignando array (Add carrito)
+console.log(vehicles);
+vehicles.pop(); //Reasignando de nuevo array (remove carrito)
+console.log(vehicles);
