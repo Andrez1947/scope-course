1adde4ad21be66e15f9d1686d17df2b07538307d
Andres Felipe Valencia Benjumea <andresvalencia213634@gmail.com>
2023-03-24 14:52:39 -0500

    Learning redeclaration and reasignement

---
+++ src/scope/re.js
@@ -0,0 +1,11 @@
+var firstName; //Undefined
+firstName = 'Saul Hermano'; //Asignando
+console.log(firstName);
+
+var lastName = 'Goodman'; //Declarar y asignar
+lastName = 'Mcgill'; //Reasignar
+console.log(lastName);
+
+var secondName = 'Jimmy'; //Declarar y asignar
+var secondName = 'Kim'; //Reasignando
+console.log(secondName);
